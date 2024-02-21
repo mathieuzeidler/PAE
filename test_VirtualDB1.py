7d00527ad6fc0d9012972cf177c0ebf32ef72462
joanMCanals <joanforply@gmail.com>
2024-02-21 16:42:39 +0100

    k

--- test_VirtualDB1.py
+++ test_VirtualDB1.py
@@ -11,13 +11,25 @@ if not os.path.exists(DOWNLOAD_DIR):
 testObj = vd.read_vital("VitalDB_data/VitalDB_data/230602/QUI12_230602_194231.vital")
 test = testObj.get_track_names()
 setD = list()
-oo = testObj.to_numpy("MedibusX/PAMB_MBAR",None)
 for f in test:
     setD.append((f,testObj.to_numpy(f,None)))
-    
-ppp = setD[0]
-x = np.arange(0,len(ppp[1]))
-print(ppp[1])
-plt.figure(1)
-plt.plot(x,ppp[1])
+
+k = 1
+for dataPair in setD:
+    x = np.arange(0,len(dataPair[1]))
+    print(dataPair[1])
+    plt.figure(k)
+    plt.stem(x,dataPair[1],linefmt='grey', markerfmt='')
+    plt.xlabel("t")
+    plt.ylabel(dataPair[0])
+    plt.title(dataPair[0])
+    #plt.plot(x,dataPair[1],'-o')
+    k = k+1
 plt.show()
+
+vital1T = vd.read_vital("1.vital")
+vital1TrackNames = vital1T.get_track_names()
+vital1Tset = list()
+vital1Tpandas
+for f in test:
+    setD.append((f,vital1T.to_numpy(f,None)))
