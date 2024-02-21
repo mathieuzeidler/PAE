3370ba8bf90841dbb58cd96aa5298e15ae6c844c
joanMCanals <joanforply@gmail.com>
2024-02-21 09:29:28 +0100

    fixed directory

--- test_VirtualDB1.py
+++ test_VirtualDB1.py
@@ -4,11 +4,11 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-DOWNLOAD_DIR = "dades/VitalDB_data/230602"
+DOWNLOAD_DIR = "VitalDB_data/VitalDB_data/230602"
 if not os.path.exists(DOWNLOAD_DIR):
     print("Creating new directory")
     os.mkdir(DOWNLOAD_DIR)
-testObj = vd.read_vital("dades/VitalDB_data/230602/QUI12_230602_194231.vital")
+testObj = vd.read_vital("VitalDB_data/VitalDB_data/230602/QUI12_230602_194231.vital")
 test = testObj.get_track_names()
 setD = list()
 oo = testObj.to_numpy("MedibusX/PAMB_MBAR",None)
@@ -18,3 +18,4 @@ for f in test:
 ppp = setD[0]
 x = np.arange(0,len(ppp[1]))
 plt.plot(x,ppp[1])
+plt.show()
